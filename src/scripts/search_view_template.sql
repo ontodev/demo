75ad70657ff8459a2ed34ab2790d5bc76f02ee50
Michael Cuffaro <consulting@michaelcuffaro.com>
2022-10-12 13:07:08 +0200

    support postgresql syntax; add missing synonym subjects; change view to table

--- src/scripts/search_view_template.sql
+++ src/scripts/search_view_template.sql
@@ -1,12 +1,12 @@
 -- Create a view with search labels as LABEL - SYNONYM [TERM ID]
-DROP VIEW IF EXISTS TABLE_NAME_search_view;
-CREATE VIEW TABLE_NAME_search_view AS
+DROP TABLE IF EXISTS TABLE_NAME_search_view;
+CREATE TABLE TABLE_NAME_search_view AS
 WITH term_ids AS (
     SELECT * FROM (
         SELECT DISTINCT subject AS subject FROM TABLE_NAME
         UNION
         SELECT DISTINCT predicate FROM TABLE_NAME
-    )
+    ) AS t
 ),
 labels AS (
     SELECT DISTINCT subject, object
@@ -14,32 +14,31 @@ labels AS (
 ),
 synonyms AS (
     SELECT * FROM (
-        SELECT DISTINCT subject, object FROM TABLE_NAME
-        WHERE predicate = 'IAO:0000118'
-        UNION
-        -- IEDB alternative term
-        SELECT DISTINCT subject, object FROM TABLE_NAME
-        WHERE predicate = 'OBI:9991118'
-        UNION
-        -- ISA alternative term
-        SELECT DISTINCT subject, object FROM TABLE_NAME
-        WHERE predicate = 'OBI:0001847'
-        UNION
-        -- NIAID GSCID-BRC alternative term
-        SELECT DISTINCT subject, object FROM TABLE_NAME
-        WHERE predicate = 'OBI:0001886'
-    )
+      SELECT DISTINCT subject, object
+        FROM TABLE_NAME
+       WHERE predicate IN (
+         -- OBI synonym terms:
+         'IAO:0000118',
+         'OBI:9991118', -- IEDB alternative term
+         'OBI:0001847', -- ISA alternative term
+         'OBI:0001886', -- NIAID GSCID-BRC alternative term
+         -- NCBITAXON, COB, and UBERON synonym terms
+         'oio:hasExactSynonym',
+         'oio:hasRelatedSynonym',
+         'oio:hasBroadSynonym'
+       )
+    ) AS t
 )
 SELECT
     t.subject AS subject,
-    COALESCE(l.object, "") || COALESCE(" - " || s.object, "") || " [" || t.subject || "]" AS label
+    COALESCE(l.object, '') || COALESCE(' - ' || s.object, '') || ' [' || t.subject || ']' AS label
 FROM term_ids t
 LEFT JOIN labels l ON t.subject = l.subject
 LEFT JOIN synonyms s ON t.subject = s.subject
 UNION
 SELECT
     t.subject AS subject,
-    l.object || " [" || t.subject || "]" AS label
+    l.object || ' [' || t.subject || ']' AS label
 FROM term_ids t
 JOIN labels l ON t.subject = l.subject
 WHERE t.subject IN (SELECT subject FROM synonyms);
